908637fd03703d8e093222d7be60216f547a0dc2
Hristo <sandevh@gmail.com>
2025-01-26 22:24:39 -0500

    parser

--- src/java/parser/Parser.java
+++ src/java/parser/Parser.java
@@ -115,44 +115,400 @@ public class Parser  extends CompilerPass {
         }
         return false;
     }
-
-
+    /**
+     * program ::= (include)* (structdecl | vardec_funcdec_or_funcdef)* EOF
+     */
     private void parseProgram() {
         parseIncludes();
 
+        // (structdecl | vardec_funcdec_or_funcdef)*
         while (accept(Category.STRUCT, Category.INT, Category.CHAR, Category.VOID)) {
-            if (token.category == Category.STRUCT &&
-                    lookAhead(1).category == Category.IDENTIFIER &&
-                    lookAhead(2).category == Category.LBRA) {
+            if (token.category == Category.STRUCT
+                    && lookAhead(1).category == Category.IDENTIFIER
+                    && lookAhead(2).category == Category.LBRA) {
+                // structdecl
                 parseStructDecl();
-            }
-            else {
-                // to be completed ...
-                nextToken(); // this line should be modified/removed
+            } else {
+                // vardec_funcdec_or_funcdef
+                parseType();               // type
+                expect(Category.IDENTIFIER); // the variable/function name
+
+                // If next token is '(', it's a function decl or def
+                if (accept(Category.LPAR)) {
+                    parseFuncDefOrDecl();  // "(" params ")" (block | ";")
+                } else {
+                    parseVarDecl();        // matrix_brackets ";"
+                }
             }
         }
-        // to be completed ...
 
         expect(Category.EOF);
     }
 
-    // includes are ignored, so does not need to return an AST node
+    /**
+     * include ::= "#include" STRING_LITERAL
+     */
     private void parseIncludes() {
-        if (accept(Category.INCLUDE)) {
-            nextToken();
+        // Just skip them recursively
+        while (accept(Category.INCLUDE)) {
+            nextToken(); // consume '#include'
             expect(Category.STRING_LITERAL);
-            parseIncludes();
         }
     }
 
-    private void parseStructDecl(){
+    // ------------------------------------------------------------------------
+    // STRUCT DECLARATIONS
+    // ------------------------------------------------------------------------
+
+    /**
+     * structdecl ::= structtype "{" (vardecl)+ "}" ";"
+     * structtype ::= "struct" IDENT
+     */
+    private void parseStructDecl() {
+        // Already verified in parseProgram that next token is 'struct'
         expect(Category.STRUCT);
         expect(Category.IDENTIFIER);
         expect(Category.LBRA);
-        // to be completed ...
+
+        // (vardecl)+  --> at least one
+        // But each field in struct: "type IDENT matrix_brackets ';'"
+        // So we parse repeated sequences of (type IDENT varDecl) until we see '}'
+        do {
+            if (accept(Category.RBRA)) break;  // stop if "}"
+            parseType();
+            expect(Category.IDENTIFIER);
+            parseVarDecl(); // matrix brackets + ';'
+        } while (!accept(Category.RBRA, Category.EOF));
+
+        expect(Category.RBRA);
+        expect(Category.SC);
     }
 
+    // ------------------------------------------------------------------------
+    // TYPE & DECLARATIONS
+    // ------------------------------------------------------------------------
 
+    /**
+     * type ::= ("int" | "char" | "void" | structtype) ("*")*
+     */
+    private void parseType() {
+        // base type
+        if (accept(Category.INT, Category.CHAR, Category.VOID)) {
+            nextToken();
+        } else if (accept(Category.STRUCT)) {
+            nextToken(); // 'struct'
+            expect(Category.IDENTIFIER);
+        } else {
+            error(Category.INT, Category.CHAR, Category.VOID, Category.STRUCT);
+            // Attempt to recover:
+            nextToken();
+        }
+        // parse zero or more '*' pointers
+        while (accept(Category.ASTERISK)) {
+            nextToken();
+        }
+    }
 
-    // to be completed ...
+    /**
+     * matrix_brackets ::= ("[" INT_LITERAL "]")*
+     */
+    private void parseMatrixSqrBrckts() {
+        while (accept(Category.LSBR)) {
+            nextToken();
+            expect(Category.INT);
+            expect(Category.RSBR);
+        }
+    }
+
+    /**
+     * vardecl ::= matrix_brackets ";"
+     * i.e., we assume 'type' and 'IDENTIFIER' are already consumed.
+     */
+    private void parseVarDecl() {
+        parseMatrixSqrBrckts();
+        expect(Category.SC);
+    }
+
+    // ------------------------------------------------------------------------
+    // FUNCTION DECLARATION OR DEFINITION
+    // ------------------------------------------------------------------------
+
+    /**
+     * func_def_or_decl ::= "(" params ")" (block | ";")
+     */
+    private void parseFuncDefOrDecl() {
+        expect(Category.LPAR);
+        parseParams();
+        expect(Category.RPAR);
+        // If we see ';' => function declaration, else we parse a block => definition
+        if (accept(Category.SC)) {
+            // function declaration
+            nextToken();
+        } else if (accept(Category.LBRA)) {
+            // function definition
+            parseBlock();
+        } else {
+            error(Category.SC, Category.LBRA);
+        }
+    }
+
+    /**
+     * params ::= [ type IDENT matrix_brackets ("," type IDENT matrix_brackets)* ]
+     */
+    private void parseParams() {
+        // Possibly empty parameter list
+        if (!accept(Category.INT, Category.CHAR, Category.VOID, Category.STRUCT)) {
+            return; // empty
+        }
+        // parse first param
+        parseType();
+        expect(Category.IDENTIFIER);
+        parseMatrixSqrBrckts();
+        // parse additional params ("," type IDENT matrix_brackets)*
+        while (accept(Category.COMMA)) {
+            nextToken(); // consume ','
+            parseType();
+            expect(Category.IDENTIFIER);
+            parseMatrixSqrBrckts();
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // BLOCKS & STATEMENTS
+    // ------------------------------------------------------------------------
+
+    /**
+     * block ::= "{" (type IDENT vardecl)* (stmt)* "}"
+     */
+    private void parseBlock() {
+        expect(Category.LBRA);
+
+        // zero or more local var declarations: (type IDENT varDecl)*
+        while (accept(Category.INT, Category.CHAR, Category.VOID, Category.STRUCT)) {
+            // Look ahead to see if next is IDENT => yes => must be varDecl
+            parseType();
+            expect(Category.IDENTIFIER);
+            parseVarDecl();
+        }
+
+        // then zero or more statements
+        while (!accept(Category.RBRA, Category.EOF)) {
+            parseStmt();
+        }
+
+        expect(Category.RBRA);
+    }
+
+    /**
+     * stmt ::=
+     *    block
+     *  | "while" "(" exp ")" stmt
+     *  | "if" "(" exp ")" stmt ["else" stmt]
+     *  | "return" [exp] ";"
+     *  | exp ";"
+     *  | "continue" ";"
+     *  | "break" ";"
+     */
+    private void parseStmt() {
+        if (accept(Category.LBRA)) {
+            // block
+            parseBlock();
+
+        } else if (accept(Category.WHILE)) {
+            nextToken(); // consume 'while'
+            expect(Category.LPAR);
+            parseExp();
+            expect(Category.RPAR);
+            parseStmt();
+
+        } else if (accept(Category.IF)) {
+            nextToken(); // 'if'
+            expect(Category.LPAR);
+            parseExp();
+            expect(Category.RPAR);
+            parseStmt();
+            if (accept(Category.ELSE)) {
+                nextToken();
+                parseStmt();
+            }
+
+        } else if (accept(Category.RETURN)) {
+            nextToken(); // 'return'
+            if (!accept(Category.SC)) {
+                parseExp();
+            }
+            expect(Category.SC);
+
+        } else if (accept(Category.CONTINUE)) {
+            nextToken();
+            expect(Category.SC);
+
+        } else if (accept(Category.BREAK)) {
+            nextToken();
+            expect(Category.SC);
+
+        } else {
+            // expression statement
+            parseExp();
+            expect(Category.SC);
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // EXPRESSIONS
+    // ------------------------------------------------------------------------
+    //
+    // According to the left-parseable grammar:
+    //
+    // exp ::= "(" ( exp ")" | type ")" exp ) exp'
+    //       | INT_LITERAL exp'
+    //       | func_call_or_var exp'
+    //       | ("-" | "+") exp exp'
+    //       | CHAR_LITERAL exp'
+    //       | STRING_LITERAL exp'
+    //       | valueat exp'
+    //       | addressof exp'
+    //       | sizeof exp'
+    //
+    // exp' ::= "=" exp exp'
+    //        | "[" exp "]" exp'
+    //        | "." IDENT exp'
+    //        | (">" | "<" | ">=" | "<=" | "!=" | "==" | "+" | "-" | "/" | "*" | "%" | "||" | "&&") exp exp'
+    //        | ε
+    //
+    // func_call_or_var ::= IDENT ["(" [ exp ("," exp)* ] ")"]
+    // valueat          ::= "*" exp
+    // addressof        ::= "&" exp
+    // sizeof           ::= "sizeof" "(" type ")"
+    //
+
+    private void parseExp() {
+        if (accept(Category.LPAR)) {
+            // Could be parenthesized exp => ( exp ) exp'
+            // or a cast => ( type ) exp exp'
+            nextToken(); // consume '('
+            // Heuristic: if next token is a type (int/char/void/struct) => parse cast
+            if (accept(Category.INT, Category.CHAR, Category.VOID, Category.STRUCT)) {
+                // cast: "(" type ")" exp
+                parseType();
+                expect(Category.RPAR);
+                parseExp();
+                parseExpPrime();
+            } else {
+                // parenthesized expression: "(" exp ")"
+                parseExp();
+                expect(Category.RPAR);
+                parseExpPrime();
+            }
+        }
+        else if (accept(Category.INT_LITERAL)) {
+            nextToken();
+            parseExpPrime();
+        }
+        else if (accept(Category.IDENTIFIER)) {
+            parseFuncCallOrVar();
+            parseExpPrime();
+        }
+        else if (accept(Category.MINUS, Category.PLUS)) {
+            // unary +/- then an expression
+            nextToken();
+            parseExp();
+            parseExpPrime();
+        }
+        else if (accept(Category.CHAR_LITERAL)) {
+            nextToken();
+            parseExpPrime();
+        }
+        else if (accept(Category.STRING_LITERAL)) {
+            nextToken();
+            parseExpPrime();
+        }
+        else if (accept(Category.ASTERISK)) { // valueAt
+            nextToken();
+            parseExp();
+            parseExpPrime();
+        }
+        else if (accept(Category.AND)) { // addressOf
+            nextToken();
+            parseExp();
+            parseExpPrime();
+        }
+        else if (accept(Category.SIZEOF)) {
+            nextToken(); // 'sizeof'
+            expect(Category.LPAR);
+            parseType();
+            expect(Category.RPAR);
+            parseExpPrime();
+        }
+        else {
+            // error or epsilon not allowed at top-level exp
+            error(Category.LPAR, Category.INT_LITERAL, Category.IDENTIFIER,
+                    Category.MINUS, Category.PLUS, Category.CHAR_LITERAL,
+                    Category.STRING_LITERAL, Category.ASTERISK, Category.AND,
+                    Category.SIZEOF);
+            // Attempt naive recovery
+            nextToken();
+        }
+    }
+
+    private void parseExpPrime() {
+        // exp' suffix expansions
+        // = "=" exp exp'
+        // = "[" exp "]" exp'
+        // = "." IDENT exp'
+        // = binop => parseExp() parseExpPrime()
+        // or epsilon
+        if (accept(Category.ASSIGN)) {  // '='
+            nextToken();
+            parseExp();
+            parseExpPrime();
+        }
+        else if (accept(Category.LSBR)) { // '['
+            nextToken();
+            parseExp();
+            expect(Category.RSBR);
+            parseExpPrime();
+        }
+        else if (accept(Category.DOT)) {  // '.'
+            nextToken();
+            expect(Category.IDENTIFIER);
+            parseExpPrime();
+        }
+        else if (accept(Category.GT, Category.LT, Category.GE, Category.LE,
+                Category.NE, Category.EQ,
+                Category.PLUS, Category.MINUS, Category.DIV, Category.ASTERISK, Category.REM,
+                Category.LOGAND, Category.LOGOR)) {
+            // binary operator
+            nextToken();
+            parseExp();
+            parseExpPrime();
+        }
+        else {
+            // ε (do nothing)
+        }
+    }
+
+    /**
+     * func_call_or_var ::= IDENT [ "(" [ exp ("," exp)* ] ")" ]
+     * We assume current token is IDENT already.
+     */
+    private void parseFuncCallOrVar() {
+        expect(Category.IDENTIFIER);  // Already matched but to consume it
+        if (accept(Category.LPAR)) {
+            // function call
+            nextToken(); // consume '('
+            if (!accept(Category.RPAR)) {
+                // parse first expr
+                parseExp();
+                // parse subsequent exprs if comma appears
+                while (accept(Category.COMMA)) {
+                    nextToken();
+                    parseExp();
+                }
+            }
+            expect(Category.RPAR);
+        }
+        // else it's just a variable usage; no further tokens
+    }
+
 }
