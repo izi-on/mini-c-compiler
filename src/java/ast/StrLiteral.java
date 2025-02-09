8459129c744fda4618bf4db32e715eaf127614f0
Hristo <sandevh@gmail.com>
2025-02-09 13:43:02 -0500

    fix str literal escape character parsing

--- src/java/ast/StrLiteral.java
+++ src/java/ast/StrLiteral.java
@@ -1,13 +1,17 @@
 package ast;
 
+import util.Unescape;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public final class StrLiteral extends Expr {
     String value;
 
-    public StrLiteral(String value) {
-        this.value = value;
+    public StrLiteral(String data) {
+        // data can be an escape sequence like \n
+        // so, parse
+        this.value = Unescape.unescape(data);
     }
 
     @Override
